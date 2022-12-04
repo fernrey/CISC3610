825b8b9f9aba9005a1e327828e58da72ab298acf
fernrey <Fernando39@live.com>
2022-12-04 15:40:04 -0500

    added more columns for new assignments

--- Fruit_Lab/chart.js
+++ Fruit_Lab/chart.js
@@ -1,7 +1,7 @@
 let fruits = [
     {name:"Strawberry", quantity:15, color:"red"},
 	{name:"Peach", quantity:10, color:"orange"},
-    {name: "Banana",quantity: 25, color: "yellow"},
+    {name: "Banana", quantity: 25, color: "yellow"},
     {name: "Pear", quantity: 10, color: "green"},
     {name: "Grape",quantity: 20, color: "purple"}
 ];
@@ -20,7 +20,7 @@ function drawRectangles() {
     if (canvas.getContext) {
         var ctx = canvas.getContext('2d');
 
-        //Data for Pear - Quantity 7 - Color Green
+        //Data for Strawberry - Quantity 7 - Color Green
         ctx.fillStyle = fruits[0].color;
         ctx.fillRect(100, 110, fruits[0].quantity * 50, 50);
         ctx.font = "40px Ariel";
@@ -28,7 +28,7 @@ function drawRectangles() {
         ctx.fillStyle = "#FFFFFF";
         ctx.fillText(fruits[0].quantity, fruits[0].quantity * 50, 145);
 
-        // Data for Strawberry - Quantity 20 - Color Red
+        // Data for Peach - Quantity 20 - Color Red
         ctx.fillStyle = fruits[1].color;
         ctx.fillRect(100, 215, fruits[1].quantity * 50, 50);
         ctx.font = "40px Ariel";
@@ -36,15 +36,15 @@ function drawRectangles() {
         ctx.fillStyle = "#FFFFFF";
         ctx.fillText(fruits[1].quantity, fruits[1].quantity * 50, 250);
 
-        //Data for Orange - Quantity 10, color Orange
+        //Data for Banana - Quantity 10, color Orange
         ctx.fillStyle = fruits[2].color;
         ctx.fillRect(100, 315, fruits[2].quantity * 50, 50);
         ctx.font = "40px Ariel ";
-        ctx.fillText(fruits[2].name, 1125, 350);
+        ctx.fillText(fruits[2].name, 1450, 350);
         ctx.fillStyle = "#FFFFFF";
         ctx.fillText(fruits[2].quantity, fruits[2].quantity * 50, 350);
 
-        //Data for Banana - Quantity 15, color Yellow
+        //Data for Pear - Quantity 15, color Yellow
         ctx.fillStyle = fruits[3].color;
         ctx.fillRect(100, 415, fruits[3].quantity * 50, 50, );
         ctx.font = "40px Ariel";
@@ -52,20 +52,12 @@ function drawRectangles() {
         ctx.fillStyle = "#FFFFFF";
         ctx.fillText(fruits[3].quantity, fruits[3].quantity * 50, 450);
 
-        //Data for Pear - Quantity 5 - Color Green
+        //Data for Grape - Quantity 5 - Color Green
         ctx.fillStyle = fruits[4].color;
         ctx.fillRect(100, 515, fruits[4].quantity * 50, 50);
         ctx.font = "40px Ariel";
         ctx.fillText(fruits[4].name, 1125, 550);
         ctx.fillStyle = "#FFFFFF";
         ctx.fillText(fruits[4].quantity, fruits[4].quantity * 50, 550);
-
-        //Data for Grape - Quantity 8 - Color Magenta
-        ctx.fillStyle = fruits[5].color;
-        ctx.fillRect(100, 615, fruits[5].quantity * 50, 50);
-        ctx.font = "40px Ariel";
-        ctx.fillText(fruits[5].name, 1125, 650);
-        ctx.fillStyle = "#FFFFFF";
-        ctx.fillText(fruits[5].quantity, fruits[5].quantity * 50, 650);
     }
 }
