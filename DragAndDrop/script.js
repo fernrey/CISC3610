1e0602fd33c5d104ce9ca4507aa9f27eeb5d5fdd
fernrey <Fernando39@live.com>
2022-12-08 20:30:22 -0500

    new lab

---
+++ DragAndDrop/script.js
@@ -0,0 +1,43 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 700,
+    autoCenter: true,
+    backgroundColor: '#ffffcc',
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('goku', 'assets/goku.png');
+    this.load.image('frieza', 'assets/Frieza.png');
+    this.load.image('jiren', 'assets/jiren.png');
+    this.load.image('tournament', 'assets/tournament.png');
+}
+
+function create() {
+
+    this.add.image(400, 300, 'tournament');
+
+    var goku = this.add.sprite(100, 660, 'goku');
+    var frieza = this.add.sprite(300, 700, 'frieza');
+    var jiren = this.add.sprite(600, 690, 'jiren');
+
+    goku.setInteractive();
+    frieza.setInteractive();
+    jiren.setInteractive();
+
+    this.input.setDraggable(goku);
+    this.input.setDraggable(frieza);
+    this.input.setDraggable(jiren);
+
+    this.input.on('drag', function(pointer, gameObject, dragX, dragY) {
+        gameObject.x = dragX;
+        gameObject.y = dragY;
+
+    });
+}
